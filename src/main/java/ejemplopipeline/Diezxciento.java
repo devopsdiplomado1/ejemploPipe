a9ebea16eff847010cf3636ac4c9e270b2ab9a7e
Rodrigo Zuñiga <rodrigozunigab@gmail.com>
2021-01-19 20:59:00 -0300

    commit inicial test

--- src/main/java/ejemplopipeline/Diezxciento.java
+++ src/main/java/ejemplopipeline/Diezxciento.java
@@ -6,34 +6,38 @@ public class Diezxciento {
 	public int sueldo;
 	public int ciframinima;
 	public int tramo1;
+	public int tramo2;
+	public double retiro;
+	public double impuesto;
 
 	
 	
 	public Diezxciento(){
 		ahorro = 0;
 		sueldo = 0;
-		ciframinima = 1000000;
-		tramo1 = 10000000;
-		tramo2 = 43000000;
+
 	}
 	
 	public Diezxciento(int ahorro, int sueldo) {
 		this.ahorro = ahorro;
 		this.sueldo = sueldo;
+		ciframinima = 1000000;
+		tramo1 = 10000000;
+		tramo2 = 43000000;
 	}
 	
 	public int diezxciento(){
-		double diezxciento;
+		
 		if (ahorro<=ciframinima)
-			diezxciento = ahorro;
+			retiro = ahorro;
 		else if (ahorro > ciframinima && ahorro <= tramo1 )
-			diezxciento =ciframinima;
+			retiro =ciframinima;
 		else if (ahorro > tramo1 && ahorro <= tramo2)
-			diezxciento = ahorro * 0.1;
+			retiro = ahorro * 0.1;
 		else 
-			diezxciento =  tramo2 * 0.1;	
+			retiro =  tramo2 * 0.1;	
 
-		return (int) diezxciento;
+		return (int) retiro;
 	}
 	
 	public int impuesto(){
@@ -46,13 +50,31 @@ TR: $2.500.000 - 3.000.000 | RP: $3.815.984 | IMP: $364.086
 TR: $3.000.000 - $4.000.000 | RP: $3.815.984 | IMP: $612.396
 TR: $4.000.000 - $5.000.000 | RP: $3.815.984 | IMP: 980.357
 TR: $5.000.000 - $6.000.000 | RP: $3.815.984 | IMP: 1.294.052
-TR: $6.000.000 y más | RP: $3.815.984 | IMP: $1.491.042*/ 	
-		return 0;
+TR: $6.000.000 y más | RP: $3.815.984 | IMP: $1.491.042*/ 
+		
+		if ( sueldo >= 0 && sueldo < 1500000)
+			impuesto = 0;
+		else if ( sueldo >= 1500000  && sueldo < 2500000)
+			impuesto = 	174531;
+		else if ( sueldo >= 2500000  && sueldo < 3000000)
+			impuesto = 	364086;	
+		else if ( sueldo >= 3000000  && sueldo < 4000000)
+			impuesto = 	612396;	
+		else if ( sueldo >= 4000000  && sueldo < 5000000)
+			impuesto = 	980357;				
+		else if ( sueldo >= 5000000  && sueldo < 6000000)
+			impuesto = 	1294052;	
+		else if ( sueldo >= 6000000)
+			impuesto = 	1491042;
+
+		return (int) impuesto;
 	}
 	
 	public int saldoAhorro() {
-		double noventaxciento = ahorro * 0.9;
-		return (int) noventaxciento;
+		diezxciento();
+		impuesto();
+
+		return (int) (ahorro - retiro - impuesto);
 	}
 
 }
--- src/test/java/ejemplopipeline/DiezxcientoTest.java
+++ src/test/java/ejemplopipeline/DiezxcientoTest.java
@@ -10,22 +10,84 @@ public class DiezxcientoTest {
 	//cesar
 	@Test
 	public void testDiezxciento() throws Exception {
-		Diezxciento diezxciento = new Diezxciento(23000000,900000);
-		assertEquals(2300000, diezxciento.diezxciento());
+		Diezxciento diezxciento = new Diezxciento(1000000,1000000);
+		assertEquals(1000000, diezxciento.diezxciento());
+	}
+	@Test
+	public void testDiezxcientoAhorroInferiorMinimo() throws Exception {
+		Diezxciento diezxciento = new Diezxciento(990000,900000);
+		assertEquals(990000, diezxciento.diezxciento());
 	}
 
-	//andres
 	@Test
+	public void testDiezxcientoAhorroIgualMinimo() throws Exception {
+		Diezxciento diezxciento = new Diezxciento(1000000,700000);
+		assertEquals(1000000, diezxciento.diezxciento());
+	}
+
+	@Test
+	public void testDiezxcientoAhorroIgualMaximo() throws Exception {
+		Diezxciento diezxciento = new Diezxciento(43000000,1200000);
+		assertEquals(4300000, diezxciento.diezxciento());
+	}
+
+	@Test
+	public void testDiezxcientoAhorroSobreMaximo() throws Exception {
+		Diezxciento diezxciento = new Diezxciento(43000001,2200000);
+		assertEquals(4300000, diezxciento.diezxciento());
+	}
+
+
+	//andres
+
+
+@Test
 	public void testImpuesto() throws Exception {
-		Diezxciento diezxciento = new Diezxciento(23000000,900000);
+		Diezxciento diezxciento = new Diezxciento(1000000,1000000);
 		assertEquals(0, diezxciento.impuesto());
+
+	    diezxciento = new Diezxciento(23000000,400000);
+		assertEquals(0, diezxciento.impuesto());
+
+		diezxciento = new Diezxciento(23000000,1600000);
+		assertEquals(174531, diezxciento.impuesto());
+
+		diezxciento = new Diezxciento(23000000,2600000);
+		assertEquals(364086, diezxciento.impuesto());
+
+		diezxciento = new Diezxciento(23000000,3500000);
+		assertEquals(612396, diezxciento.impuesto());
+
+		diezxciento = new Diezxciento(23000000,4200000);
+		assertEquals(980357, diezxciento.impuesto());
+
+		diezxciento = new Diezxciento(23000000,5400000);
+		assertEquals(1294052, diezxciento.impuesto());
+
+		diezxciento = new Diezxciento(23000000,6100000);
+		assertEquals(1491042, diezxciento.impuesto());
+
 	}
 
 	//tamara
 	@Test
 	public void testSaldoAhorro() throws Exception {
-		Diezxciento diezxciento = new Diezxciento(23000000,900000);
+		Diezxciento diezxciento = new Diezxciento(1000000,1000000);
+		assertEquals(0, diezxciento.saldoAhorro());
+
+	    diezxciento = new Diezxciento(23000000,900000);
 		assertEquals(20700000, diezxciento.saldoAhorro());
+
+
+
+		diezxciento = new Diezxciento(45000000,2500000);
+		assertEquals(40500000, diezxciento.saldoAhorro());
+
+		diezxciento = new Diezxciento(1000000,500000);
+		assertEquals(900000, diezxciento.saldoAhorro());
+
+		diezxciento = new Diezxciento(6500000,1500000);
+		assertEquals(5850000, diezxciento.saldoAhorro());	
 	}
 
 	//hacer mas assert
